03171f41f75cd9409b29af3329a4b0fe001dc449
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-01 13:53:19 -0500

    finished first scanner class exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main (String args[]) {
+        Scanner scanner = new Scanner(System.in);
         double pi = 3.14159;
         System.out.format("The value of pi is approximately "  + "%.2f%n", pi);
+        System.out.println("Please enter an integer: ");
+        int aNumber = scanner.nextInt();
+        System.out.printf("%d is your number." , aNumber);
+
     }
 }
