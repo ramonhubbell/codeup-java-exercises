d544746b7fcb15f1f10dbc95991105532361d814
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-01 10:33:45 -0500

    finished exercises four through seven

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,8 +5,23 @@ public class HelloWorld {
         byte myFavoriteNumber = 7;
         System.out.println("myFavoriteNumber " + myFavoriteNumber);
 
-        char myString = 'R';
-        System.out.println("myString " + myString);
+//        String myString = "Ramon";
+//        System.out.println("myString " + myString);
+//
+//        char myString = 'R';
+//        System.out.println("myString " + myString);
+
+        double myString = 3.14159;
+          System.out.println("myString " + myString);
+
+//        long myNumber =
+//                System.out.println("myNumber " + myNumber);
+
+          double myNumber = 3.14;
+          System.out.println("myNumber " + myNumber);
+
+//        long myNumber = 123L;
+//        System.out.println("myNumber " + myNumber);
 
 
     }
