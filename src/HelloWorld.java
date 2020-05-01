8c87a030185806505822af612e1f268247f125b4
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-01 11:56:31 -0500

    working on exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -31,5 +31,13 @@ public class HelloWorld {
 
 //            double myNumber = 3.14;
 //            System.out.println("myNumber " + myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
     }
 }
