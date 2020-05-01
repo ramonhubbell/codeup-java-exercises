5fbe969ab0cf7b10a199aa44ccb1767f28b2d825
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-01 10:16:42 -0500

    finished execise three

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,11 +2,12 @@ public class HelloWorld {
     public static void main (String args[]) {
         System.out.println("Hello World!");
 
-        byte myNumber = 7;
-        System.out.println("myNumber " + myNumber);
+        byte myFavoriteNumber = 7;
+        System.out.println("myFavoriteNumber " + myFavoriteNumber);
 
-        String myName = "Ramon";
-        System.out.println("myName " + myName);
+        char myString = 'R';
+        System.out.println("myString " + myString);
+
 
     }
 }
