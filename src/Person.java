8d5a60132d639db3be1285948aad8047202b6a72
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-07 16:47:15 -0500

    finished first two exercises

---
+++ src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+//    Creating fields.
+
+    private String name;
+
+//    Constructor
+
+    public Person ( String n ) {
+    this.name = n;
+    }
+
+//    The plan template.
+
+    // returns the person's name
+    public String getName() {
+    return name;
+    };
+    // changes the name property to the passed value
+    public void setName(String name) {
+    this.name = name;
+    };
+    // prints a message to the console using the person's name
+    public void sayHello() {
+    System.out.println("Hello" + name + ". Welcome to Object Oriented Programming!");
+    };
+
+}
