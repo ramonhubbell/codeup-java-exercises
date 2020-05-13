a16ff1580b95d81bb7d4033277a6868a5b41160b
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-05-12 23:34:40 -0500

    finished exercises one through four

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,29 +1,29 @@
 package shapes;
 
-public class Rectangle {
+public class Rectangle extends Quadrilateral implements Measurable{
 //    It should have protected properties for both length and width.
-    protected double length;
-    protected double width;
+//    protected double length;
+//    protected double width;
 
 //    Rectangle should define a constructor that accepts two numbers
 //    for length and width, and sets those properties.
-    public Rectangle (double rectangleLength, double rectangleWidth) {
-        this.length = rectangleLength;
-        this.width = rectangleWidth;
-    }
+//    public Rectangle (double rectangleLength, double rectangleWidth) {
+//        this.length = rectangleLength;
+//        this.width = rectangleWidth;
+//    }
 
 //    Create two methods on the Rectangle class, getArea and getPerimeter
 //    that return the respective values. The formulas for both follow:
 //    perimeter = 2 x length + 2 x width
 //    area = length x width
 
-    public double getArea () {
-        return this.length * this.width;
-    }
-
-    public double getPerimeter () {
-        return (2 * this.length) + ( 2 * this.width);
-    }
+//    public double getArea () {
+//        return this.length * this.width;
+//    }
+//
+//    public double getPerimeter () {
+//        return (2 * this.length) + ( 2 * this.width);
+//    }
 
 
 }
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,27 +1,27 @@
 package shapes;
 
-public class Square extends Rectangle {
+public class Square extends Quadrilateral {
 //    Square should define a constructor that accepts one argument,
 //    side, and calls the parent's constructor to set both the length
 //    and width to the value of side.
-    protected double side;
-
-    public Square (double squareSide) {
-        super(squareSide, squareSide);
-        this.side = squareSide;
-    }
+//    protected double side;
+//
+//    public Square (double squareSide) {
+//        super(squareSide, squareSide);
+//        this.side = squareSide;
+//    }
 
 //    In the Square class, override the getArea and getPerimeter methods
 //    with the following definitions for a square:
 //    perimeter = 4 x side
 //    area = side ^ 2
-    public double getArea () {
-        return Math.pow (super.width, 2);
-    }
-
-    public double getPerimeter () {
-        return 4 * super.length;
-    }
+//    public double getArea () {
+//        return Math.pow (super.width, 2);
+//    }
+//
+//    public double getPerimeter () {
+//        return 4 * super.length;
+//    }
 
 
 
