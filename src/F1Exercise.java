1014dd2b5a64a525d5cc25c58af4b9ac5b075764
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-08-21 16:22:03 -0500

    finished third exercise

--- src/F1Exercise.java
+++ src/F1Exercise.java
@@ -1,12 +1,56 @@
+import javax.print.attribute.IntegerSyntax;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class F1Exercise {
+
+    public static class Node {
+        int val;
+//        Integer mid;
+        Node left;
+        Node right;
+
+//        public Node() {
+//        }
+
+        public Node(int val) {
+            this.val = val;
+//            left = right = null;
+        }
+
+        public Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+
+        public int getVal() {
+            return val;
+        }
+    }
 
     public static void main(String[] args) {
 
 //        printFooBarBaz();
-        System.out.println(reverseCharacters("the cat is fat"));
-        System.out.println(reverseCharacters("band camp is great!"));
+
+//        System.out.println(reverseCharacters("the cat is fat"));
+//        System.out.println(reverseCharacters("band camp is great!"));
+//        [1,2,3,4,5,6,7
+        ArrayList<Integer> F1Numbers = new ArrayList<Integer>();
+        F1Numbers.add(1);
+        F1Numbers.add(2);
+        F1Numbers.add(3);
+        F1Numbers.add(4);
+        F1Numbers.add(5);
+        F1Numbers.add(6);
+        F1Numbers.add(7);
+//        F1Numbers.add(8);
+//        F1Numbers.add(9);
+
+        System.out.println(F1Numbers);
+        System.out.println(balancedTree(F1Numbers).getVal());
 
     }
 
@@ -24,21 +68,38 @@ public class F1Exercise {
 //        }
 //    }
 
-public static String reverseCharacters(String sentence) {
-    String [] arrayWords = sentence.split(" ");
-    String [] reversedWords = new String[arrayWords.length];
-    for(int i=0; i<arrayWords.length; i++)
-    {
-        //added for setting element as emptyString instead of null
-        reversedWords[i] = "";
-        for(int j = arrayWords[i].length() - 1; j >= 0; j--)
-        {
-            reversedWords[i] += arrayWords[i].substring( j, j + 1);
-        }
+//public static String reverseCharacters(String sentence) {
+//    String [] arrayWords = sentence.split(" ");
+//    String [] reversedWords = new String[arrayWords.length];
+//    for(int i = 0; i < arrayWords.length; i++) {
+//        reversedWords[i] = "";
+//        for(int j = arrayWords[i].length() - 1; j >= 0; j--)
+//        {
+//            reversedWords[i] += arrayWords[i].substring( j, j + 1);
+//        }
+//    }
+//    String newString = String.join (" ", reversedWords);
+//    return newString;
+//}
+
+    public static Node balancedTree(ArrayList<Integer> inputList) {
+        return helper(inputList, 0, inputList.size() - 1);
     }
-    String newString = String.join (" ", reversedWords);
-    return newString;
-}
 
+    private static Node helper(ArrayList<Integer> inputList, int low, int high) {
+        if (low > high) {
+            return null;
+        }
 
+        int mid = low + (high - low) / 2;
+//        System.out.println(mid);
+        //center val of sorted array as the root of the bst
+        Node head = new Node(inputList.get(mid));
+        System.out.println(head.val);
+        head.left = helper(inputList, low, mid - 1);
+        //        right of the mid value should go to the right of this root node to satisfy bst
+        head.right = helper(inputList, mid + 1, high);
+        return head;
+
+    }
 }
