47f199c3b59fe9eee7ae504a592e14a77fb28abe
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-10-16 18:28:27 -0500

    feature: finished third exercise

--- src/TestExercises.java
+++ src/TestExercises.java
@@ -1,6 +1,10 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class TestExercises {
 
-    static int count = 0;
+//    static int count = 0;
 
     public static void main(String[] args) {
 
@@ -9,12 +13,25 @@ public class TestExercises {
 //            System.out.println(message.charAt(i + 1));
 //        }
 
-        if(count < 3) {
-            count ++;
-            main(null);
-        }else{
-            return;
-        }
-        System.out.println("Hello World!");
+//        if(count < 3) {
+//            count ++;
+//            main(null);
+//        }else{
+//            return;
+//        }
+//        System.out.println("Hello World!");
+//        String message = "Hello";
+//        print(message);
+//        message = message + "World!";
+//        print(message);
+
+        String [] array = {"abc", "2", "10", "0"};
+        List<String> list = Arrays.asList(array);
+        Collections.sort(list);
+        System.out.println(Arrays.toString(array));
     }
+//    static void print(String message){
+//        System.out.print(message);
+//        message += " ";
+//    }
 }
