a414828f86289089ffc14a97aef23f77cfe8af69
Ramon Hubbell <Ramon.hubbell@gmail.com>
2020-10-20 21:45:35 -0500

    feature: finished nineth exercise

--- src/TestExercises.java
+++ src/TestExercises.java
@@ -8,6 +8,16 @@ public class TestExercises {
     //    static int count = 0;
     private static final Scanner scanner = new Scanner(System.in);
 
+    class Arithmetic{
+        int add(int a, int b){
+            return a + b;
+        }
+    }
+
+    class Adder extends Arithmetic{
+
+    }
+
     public static void main(String[] args) {
 
 //        String message = "Hello";
@@ -84,30 +94,32 @@ public class TestExercises {
 //        }
 //            in.close();
 
-        Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
-
-        for(int i = 0; i < t; i++) {
-            try {
-                long x = sc.nextLong();
-                System.out.println(x + " can be fitted in:");
-                if(x >= -128 && x <= 127){
-                    System.out.println("* byte");
-                }
-                //Complete the code
-                if (x >= -32768 && x <= 32767){
-                    System.out.println("* short");
-                }if (x >= -2147483648 && x <= 2147483647) {
-                    System.out.println("* int");
-                }if ( x >= -9223372036854775808L && x <= 9223372036854775807L){
-                    System.out.println("* long");
-                    }
-            }
-            catch(Exception e) {
-                System.out.println(sc.next() + " can't be fitted anywhere.");
-            }
-
-        }
+//        Scanner sc = new Scanner(System.in);
+//        int t=sc.nextInt();
+//
+//        for(int i = 0; i < t; i++) {
+//            try {
+//                long x = sc.nextLong();
+//                System.out.println(x + " can be fitted in:");
+//                if(x >= -128 && x <= 127){
+//                    System.out.println("* byte");
+//                }
+//                //Complete the code
+//                if (x >= -32768 && x <= 32767){
+//                    System.out.println("* short");
+//                }if (x >= -2147483648 && x <= 2147483647) {
+//                    System.out.println("* int");
+//                }if ( x >= -9223372036854775808L && x <= 9223372036854775807L){
+//                    System.out.println("* long");
+//                    }
+//            }
+//            catch(Exception e) {
+//                System.out.println(sc.next() + " can't be fitted anywhere.");
+//            }
+//        }
+        int mask = 0x000F;
+        int value = 0x2222;
+        System.out.println(value & mask);
         }
 //    static void print(String message){
 //        System.out.print(message);
